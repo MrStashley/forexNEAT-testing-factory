6eca15203551cf166bc31c559182e732d70f4ac7
MrStashley <colel1410@gmail.com>
2020-04-22 21:54:01 -0400

    working on capability to parse files sent

--- codeTest.py
+++ codeTest.py
@@ -1,10 +1,30 @@
 import pickle
 import neat
+import os
 
 
 def readtoTestingFacilityTest():
-    filename = "/home/ec2-user/toTestingFacility.pkl"
-    data = pickle.load(open(filename,"rb"))
+	data = []
+    files = os.scandir("/home/ec2-user/")
+
+    for file in files:
+    	if file.is_file() == False:
+    		continue
+    	try:
+    		curFiledata = pickle.load(file.path,"rb")
+    		config = curFiledata["config"]
+    		winners = curFiledata["winners"]
+    		for winner in winners:
+    			data.append({
+    				"id": winner["id"],
+    				"genome": winner[genome],
+    				"config": config
+    			})
+    	except:
+    		print("an error has occured with " + str(file.path))
+
+    	os.remove(file.path)
+    	
     print(data)
 
 def main():
