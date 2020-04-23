df2e07a553607d0cb6e074e65672096cdee7be44
MrStashley <colel1410@gmail.com>
2020-04-22 22:23:47 -0400

    messed up indentation

--- codeTest.py
+++ codeTest.py
@@ -9,19 +9,19 @@ def readtoTestingFacilityTest():
     files = os.scandir("/home/ec2-user/forexNEAT-testing-factory/toTestingFactory")
 
     for file in files:
-    if file.is_file() == False:
-        continue
-    curFiledata = pickle.load(open(file.path,"rb"))
-    config = curFiledata["config"]
-    winners = curFiledata["winners"]
-    for winner in winners:
-        data.append({
-            "id": winner["id"],
-            "genome": winner["genome"],
-            "config": config
-        })
+        if file.is_file() == False:
+            continue
+        curFiledata = pickle.load(open(file.path,"rb"))
+        config = curFiledata["config"]
+        winners = curFiledata["winners"]
+        for winner in winners:
+            data.append({
+                "id": winner["id"],
+                "genome": winner["genome"],
+                "config": config
+            })
 
-    os.remove(file.path)
+        os.remove(file.path)
 
     print(data)
     pickle.dump(data, open("testingFacilitySnakeData.pkl", "wb"))
